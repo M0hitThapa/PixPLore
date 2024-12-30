d0369f9a67c82a7239ac64c6d9ecb54e94f93ca4
Mohit Thapa <thapamohit2058@gmail.com>
2024-12-30 23:37:55 +0545

    setup database in appwrite

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -49,7 +49,7 @@ const SignupForm = () => {
 
       <div className="sm:w-420 flex-center flex-col">
         <h1 className="flex flex-col font-bold font-mono text-5xl text-teal-600 ">PixPlore</h1>
-        <h2 className="h3-bold md:h2-bold pt-5 font-mono sm:pt-12">Create a new account</h2>
+        <h2 className="h3-bold md:h2-bold pt-5 font-mono sm:pt-5">Create a new account</h2>
         <p className="text-light-3 small-medium font-mono md:base-regular mt-2">To use PixPlore, enter your account details</p>
       
       <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
