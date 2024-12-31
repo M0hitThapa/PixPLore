ed22d2619d38c79bb6ad2347de26dec258766338
Mohit Thapa <thapamohit2058@gmail.com>
2024-12-31 23:33:39 +0545

    .

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,6 +1,6 @@
 import { ID } from 'appwrite';
 import { INewUser } from "@/types";
-import { account } from './config';
+import { account, avatars } from './config';
 
 export async function createUserAccount(user: INewUser) {
 try {
@@ -9,10 +9,29 @@ try {
         user.email,
         user.password,
         user.name
-    )
-    return newAccount;
+    );
+    if(!newAccount) throw Error;
+
+    const avatarUrl = avatars.getInitials(user.name);
+
+    const newUser = await saveUserToDB({
+        accountId: newAccount.$id,
+        name: newAccount.name,
+        email: newAccount.email,
+        username: user.username,
+        imageUrl: new URL(avatarUrl),
+    })
+    return newUser;
 } catch (error) {
     console.log(error);
     return error;
 }
 }
+
+export async function saveUserToDB(user: {
+    accountId: string;
+    email: string;
+    name: string;
+    imageUrl: URL;
+    username?: string;
+})
